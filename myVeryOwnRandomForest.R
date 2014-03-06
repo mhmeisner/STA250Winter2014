4de67f7fe279f8d71525afddd2b397fb71da6f15
mhmeisner <mhmeisner@gmail.com>
2014-03-06 11:57:00 -0800

    my random forest code is done, working, and tested

--- myVeryOwnRandomForest.R
+++ myVeryOwnRandomForest.R
@@ -3,41 +3,64 @@ myRandomForest = function(form,d,ntrees=500,split_n=2,min_bin_size){
 	# d: data frame, predictor variables in form must have column with same name in d
 	# ntrees = number of trees in the forest
 	# split_n = number of predictor variables to consider splitting on at each node
-	# min_bin_size = number of training examples in each bin at which we stop splitting that bin 
+	# min_bin_size = number of training examples in each bin at which we stop splitting that bin (not implemented in this version )
 	response = as.character(form)[2]
+	
 	predictors = strsplit(as.character(form)[[3]],' \\+ ')[[1]]
 	
-	trees = sapply(1:ntrees,function(i){
+	trees_and_errors = sapply(1:ntrees,function(i){
 		# sample rows of d with replacement to get dataset to use for the ith tree 
 		w = sample(1:nrow(d),replace=T)
+		oob_indices = (1:nrow(d))[!(1:nrow(d) %in% w)]
+		oob_d = d[oob_indices,]
 		dd = d[w,] 
-		growTree(dd,response,predictors,split_n,min_bin_size)
+		tree = growTree(dd,response,predictors,split_n,min_bin_size)
+		oob_error = getOOBError(tree,oob_d,response)
+		list(tree,oob_error)
 	})
 	
+	# get estimate of oob error
+	oob_error = mean(sapply(1:ntrees,function(i){
+		trees_and_errors[2,i][[1]]
+	}))	
+	list_of_trees = trees_and_errors[1,]
+	
+	# return error and trees so we can predict 
+	list(list_of_trees,oob_error)
 }
 
 
-getOOBError = function()
-
-growTree = function(d,response,predictors,split_n,min_bin_size){
-	current_bins = list(1:nrow(d))
-	current_bin_sizes = nrow(d)
-	w = current_bin_sizes>min_bin_size
-	while(sum(w)>0){
-		for()
-	}
+getOOBError = function(t,d,response){
+	# given a tree (a list with 1st element the variable, 2nd element the cutoff, 3rd element the prediction if variable is less than the cutoff, 4th the prediction if greater than cutoff), and a oob data frame d, calculates MSE of the model predicting response for d
+	var = d[,t[[1]]]
+	pred = ifelse(var<as.numeric(t[[2]]),as.numeric(t[[3]]),as.numeric(t[[4]]))
+	actual = d[,response]
+	mse = mean((actual-pred)**2)
+	mse
 	
 }
+
+growTree = function(d,response,predictors,split_n,min_bin_size){
+	#current_bins = list(1:nrow(d))
+	#current_bin_sizes = nrow(d)
+	m = makeNode(d,response,predictors, split_n)
+	predictionIfLessThanCutoff = mean(d[,response][d[,m[[1]]]<m[[2]]])
+	predictionIfGreaterThanCutoff = mean(d[,response][d[,m[[1]]]>m[[2]]])
+	as.list(c(unlist(m),predictionIfLessThanCutoff,predictionIfGreaterThanCutoff))
+}
+
+
+growTree(d,'y',c('x1','x2','x3'),2,3)
 
 makeNode = function(d,response,predictors,split_n){
 	some_preds = sample(predictors,split_n) # stochastically choose subset of variables to consider 
 	g = getBestSplit(d,response,some_preds)
-	
+	g
 }
 
 
 
-d = data.frame(y=c(5,6,7,7,7),x1=c(2,3,3,3,3),x2=c(6,6,1,33,1),x3=c(1,1,2,2,2))
+
 
 getBestSplit = function(d,response,predictors){
 	# assume predictor is continuous and response is continuous 
@@ -77,19 +100,32 @@ getCandidateSplitPoints = function(x){
 
 getCandidateSplitPoints(x)
 
-# cat response, cat predictor (need to consider all possible subsets of categorical predictors for split? )
-# cat response, cont predictor
-# cont response, cat predictor
-# cont respones, cont predictor 
 
+# make function for prediction:
+predictRandomForest = function(trees,d){
+	predictions = sapply(trees,function(tree){
+		sapply(1:nrow(d),function(i){
+			var = d[,tree[[1]]][i]
+			pred = ifelse(var<as.numeric(tree[[2]]),as.numeric(tree[[3]]),as.numeric(tree[[4]]))
+			pred
+		})
+	})
+	rowMeans(predictions)
+}
 
-# simulate CV stuff to show my reasoning is valid!  
+# testing code 
+d = data.frame(y=rnorm(100),x1=rnorm(100,y,1),x2=rnorm(100,y,2),x3=rnorm(100))
+f = myRandomForest(y~x1+x2+x3,d,100,2,3)
+length(f)
+f[[1]]
+f[[2]] # oob error 
 
-a = rnorm(5)
-b = rnorm(5)
-lm('a~b')
-lm(a~b)
+# simulate new data under in same way, measure predictive performance: 
+d = data.frame(y=rnorm(100),x1=rnorm(100,y,1),x2=rnorm(100,y,2),x3=rnorm(100))
+p = predictRandomForest(f[[1]],d)
+plot(d$y,p,xlab='Actual',ylab='Predicted',main='Predicted vs. Actual: Simulated Data with myRandomForest')
 
+# explore how to extract vars from formula object: 
 f = formula('b~a+tt+dfadf')
 class(f)
 strsplit(as.character(f)[[3]],' \\+ ')[[1]]
@@ -107,3 +143,12 @@ b = function(y,z){
 }
 x = 4
 a(56,3,4)
+
+lala = 10
+a = function(x){
+	lala = 2
+	sapply(1:5,function(j){
+		print(lala)
+	})
+}
+a(4)
